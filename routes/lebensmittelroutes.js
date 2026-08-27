f6479cba80ada4064d8cda409b3de971c351193f
Natalia Kaczor <Natalia.Kaczor@Student.HTW-Berlin.de>
2026-08-27 18:08:14 +0200

    POST Lebensmittel erstellt

--- routes/lebensmittelroutes.js
+++ routes/lebensmittelroutes.js
@@ -8,4 +8,28 @@ router.get('/lebensmittel', async (req, res) => {
     res.send(alleLebensmittel);
 });
 
+
+// ein neues Lebensmittel eintragen 
+router.post('/', async (req, res) =>{
+    try {
+        const neuerLebensmittel = new Lebensmittel({
+            name: req.body.name,
+            kategorie: req.body.kategorie,
+            altersempfehlung: req.body.altersempfehlung,
+            allergen: req.body.allergen,
+            beschreibung: req.body.beschreibung,
+            bild: req.body.bild
+        });
+
+        const gespeichertesLebensmittel = await neuerLebensmittel.save();
+        console.log('Neues Lebensmittel erstellt:', gespeichertesLebensmittel._id);
+        res.status(201);
+        res.send(gespeichertesLebensmittel);} 
+
+    catch (error) {
+        res.status(400);
+        res.send({ error: "Das Lebensmittel konnte nicht erstellt werden." });
+    }
+});
+
 module.exports = router;
