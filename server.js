dd9e070ae2372cdc8079fdc3c5fb8bcb5682fc7a
Natalia Kaczor <Natalia.Kaczor@Student.HTW-Berlin.de>
2026-09-15 11:42:13 +0200

    Vorbereitungen zu Einbindugn + Fehlerbehebung

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routes/lebensmittelroutes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
